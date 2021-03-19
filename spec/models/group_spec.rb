9f43eacd4e2fdfe0e310dc60df12cf9d321091b7
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-03-19 13:35:06 -0400

    Add test for the group model

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:group) { FactoryBot.build(:group) }
+  it 'expect the name to be present' do
+    group.name = nil
+    expect(group).to_not be_valided
+  end
+
+  it 'expect the name to be at least 3 characters' do
+    group.name = 'ma'
+    expect(group).to_not be_valide
+  end
+
+  it 'expect the icon to be present' do
+    group.icon = nil
+    expect(group).to_not be_valide
+  end
 end
