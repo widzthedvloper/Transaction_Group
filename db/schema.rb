8ffd51ca6e0fccb4daa57de354bf08e4b89b0136
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-03-19 22:35:36 -0400

    Removed table with wrong name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_18_172142) do
+ActiveRecord::Schema.define(version: 2021_03_20_021902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,17 @@ ActiveRecord::Schema.define(version: 2021_03_18_172142) do
     t.string "icon"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
+
+  create_table "groups_transactions", force: :cascade do |t|
+    t.bigint "group_id", null: false
+    t.bigint "transaction_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["group_id"], name: "index_groups_transactions_on_group_id"
+    t.index ["transaction_id"], name: "index_groups_transactions_on_transaction_id"
   end
 
   create_table "transactions", force: :cascade do |t|
@@ -28,6 +39,8 @@ ActiveRecord::Schema.define(version: 2021_03_18_172142) do
     t.integer "amount"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_transactions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +49,8 @@ ActiveRecord::Schema.define(version: 2021_03_18_172142) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "groups", "users"
+  add_foreign_key "groups_transactions", "groups"
+  add_foreign_key "groups_transactions", "transactions"
+  add_foreign_key "transactions", "users"
 end
