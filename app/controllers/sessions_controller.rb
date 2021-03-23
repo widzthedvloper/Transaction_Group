3d112d45bf3116121d5d14e3cc36224ca0b4d6f8
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-03-23 09:29:51 -0400

    Update code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,16 @@ class SessionsController < ApplicationController
     @user = User.new
   end
 
+  def create
+    user = User.find_by(name: params[:name])
+    if user.present?
+      session[:user_id] = user.id
+      redirect_to main_path, notice: 'Log in successfully'
+    else
+      render :new
+    end
+  end
+
   def destroy
     session[:user_id] = nil
     redirect_to root_path, notice: 'Log out successfully!'
