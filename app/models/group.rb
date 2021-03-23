feac3a3cfc52685853788e92ad652dc93e8ad2a8
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-03-23 17:30:59 -0400

    Update association

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,6 @@ class Group < ApplicationRecord
   validates :name, presence: true, length: { minimum: 3, maximum: 20 }
   validates :icon, presence: true
 
-  has_and_belongs_to_many :my_transactions, class_name: 'Transaction'
+  has_and_belongs_to_many :transactions
   belongs_to :user
 end
