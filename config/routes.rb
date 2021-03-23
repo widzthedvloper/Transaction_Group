6adf70d61ed6bf5b8e6c1475fae47f24ab3428f3
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-03-23 08:09:28 -0400

    Create route to logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+  delete "log_out", to: "sessions#destroy"
   root to: "profil#index"
 end
