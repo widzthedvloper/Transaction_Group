c6b0712bacd8ef9fb52aa6fcec3d6fa7367b08e0
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-03-23 08:11:55 -0400

    Add routes to sign in

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+  
   delete "log_out", to: "sessions#destroy"
   root to: "profil#index"
 end
