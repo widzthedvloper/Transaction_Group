6e37be1954314923c84f583a7173444af2dd08f6
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-03-23 08:41:00 -0400

    Create the new action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,8 @@
 class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
   def destroy
     session[:user_id] = nil
     redirect_to root_path, notice: 'Log out successfully!'
