1433bf7e42aff3f1f09ffc373a3a30b84bb88dfd
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-03-26 19:02:53 -0400

    Remove logic in the group new view

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,15 @@
 module GroupsHelper
+  def div_group_start
+    var = '<div class="alert alert-danger" role="alert">'
+    var.html_safe if @groups.errors.any?
+  end
+
+  def div_group_end
+    var1 = '</div>'
+    var1.html_safe if @groups.errors.any?
+  end
+
+  def display_group_errors
+    @groups.errors.full_messages.each { |message| p message } if @groups.errors.any?
+  end
 end
