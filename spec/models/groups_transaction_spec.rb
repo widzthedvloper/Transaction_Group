f2d8cdd4d1c737f5783df8cde141e0011b9dd78d
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-03-26 15:47:57 -0400

    Remove pending test

--- spec/models/groups_transaction_spec.rb
+++ spec/models/groups_transaction_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe GroupsTransaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:groups_transaction) { FactoryBot.build(:groups_transaction) }
+  it 'expect the transaction_id to be present' do
+    groups_transaction.transaction_id = nil
+    expect(groups_transaction).to_not be_valid
+  end
+
+  it 'expect the group_id to be present' do
+    groups_transaction.group_id = nil
+    expect(groups_transaction).to_not be_valid
+  end
 end
